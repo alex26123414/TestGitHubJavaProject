c69565ec4e38bddd3370fb534908dd2835d9004c
alex <alex26123414@gmail.com>
2014-04-01 15:02:00 +0200

    This is a test commit. First commit

---
+++ TestGitJavaProject/src/dk/test/alex/Main.java
@@ -0,0 +1,10 @@
+package dk.test.alex;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("It's all about skills");
+	}
+
+}
